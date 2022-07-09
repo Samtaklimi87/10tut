bb1584ed38104ce36a9dc5f43631c1208ab45096
Sam Taklimi <sam.taklimi87@gmail.com>
2022-07-09 20:24:12 +0200

    adding hexcolor to show the hexa color name, adding toggle button incase the color is dark or white, able to see the color code

--- src/Square.js
+++ src/Square.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Square = ({colorValue}) => {
+const Square = ({ colorValue , hexValue , isDarkText}) => {
   return (
-    <section 
-        className='square'
-        style={{backgroundColor: colorValue}}
-        >
-        <p>{colorValue ? colorValue : 'Empty Value'}</p>
+    <section className="square" style={{ backgroundColor: colorValue , color: isDarkText ? "#000" :"#fff"}}>
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null} </p>
     </section>
-  )
-}
+  );
+};
 
 Square.defaultProps = {
-    colorValue: 'Empty Color Value'
-}
-export default Square
+  colorValue: "Empty Color Value",
+};
+export default Square;
